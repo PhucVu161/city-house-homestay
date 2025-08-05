b7826304c1b0d6b59a9233187c9dd9b4aa1c775e
PhucVu161 <phucvuhuu1601@gmail.com>
2025-08-05 22:56:28 +0700

    init backend for project with express and connect to mongoDB

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+// Routes
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/rooms', require('./routes/rooms'));
+// app.use('/api/bookings', require('./routes/bookings'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
